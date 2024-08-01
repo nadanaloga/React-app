cc1202269f7fe25b86b9440e4caaf99bbcb1f040
nadanaloga <mathanpv12@gmail.com>
2024-08-01 09:54:20 +0530

    Phone number change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,8 +11,8 @@ const Navbar = () => {
   };
 
   return (
-    <header className="fixed w-full top-0 z-50 bg-white border border-b-4">
-      <div className="container mx-auto">
+    <header className="fixed w-full top-0 z-50 bg-white border-b-4">
+      <div className="container mx-auto px-4"> {/* Add padding to the container */}
         {/* First row: logo, nav links, and social icons */}
         <div className="flex justify-between items-center py-2">
           <div className="flex items-center">
@@ -24,9 +24,6 @@ const Navbar = () => {
                 <button onClick={() => window.open("https://www.facebook.com/bharathanaatiyam?mibextid=ZbWKwL")} className="text-blue-700 hover:text-blue-900 focus:outline-none">
                   <FaFacebook size={20} style={{ color: '#1877F2' }} />
                 </button>
-                {/* <button onClick={() => window.open("https://twitter.com")} className="text-blue-400 hover:text-blue-600 focus:outline-none">
-                  <FaTwitter size={20} style={{ color: '#1DA1F2' }} />
-                </button> */}
                 <button onClick={() => window.open("https://www.instagram.com/nadanaloga_chennai?igsh=MTNva2wxNGYxaGl2Nw==")} className="text-pink-600 hover:text-pink-800 focus:outline-none">
                   <FaInstagram size={20} style={{ color: '#E1306C' }} />
                 </button>
@@ -39,16 +36,9 @@ const Navbar = () => {
           <nav className="hidden md:flex space-x-4">
             <NavLink to="/" exact activeClassName="text-blue-500" className="hover:text-pinkcolor">Home</NavLink>
             <NavLink to="/about" activeClassName="text-blue-500" className="hover:text-pinkcolor">About</NavLink>
-            {/* <NavLink to="/course" activeClassName="text-blue-500" className="hover:text-pinkcolor">Courses</NavLink> */}
             <NavLink to="/gallery" activeClassName="text-blue-500" className="hover:text-pinkcolor">Gallery</NavLink>
             <NavLink to="/contact" activeClassName="text-blue-500" className="hover:text-pinkcolor">Contact</NavLink>
-            {/* <NavLink to="/reviews" activeClassName="text-blue-500" className="hover:text-pinkcolor">Reviews</NavLink>
-            <NavLink to="/team" activeClassName="text-blue-500" className="hover:text-pinkcolor">Team</NavLink> */}
           </nav>
-          {/* <div className="hidden md:flex space-x-4">
-            <button className="bg-green-500 text-white px-2 py-1 rounded-md hover:bg-green-600">Login</button>
-            <button className="bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600">Register</button>
-          </div> */}
           <button className="md:hidden flex items-center text-blue-500 focus:outline-none" onClick={toggleMenu}>
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}></path>
@@ -58,7 +48,7 @@ const Navbar = () => {
         {/* Second row: contact information */}
         <div className="hidden md:flex justify-end items-center space-x-4 mt-2">
           <span className="flex items-center text-gray-600">
-            <FaPhone className="mr-2 font-bold"  /> +91 95 66 86 65 66
+            <FaPhone className="mr-2 font-bold" /> +91 9566866588
           </span>
           <span className="flex items-center text-gray-600">
             <FaEnvelope className="mr-2 font-bold" /> nadanalogaa@gmail.com
@@ -69,25 +59,19 @@ const Navbar = () => {
         <nav className="md:hidden bg-white shadow-md">
           <NavLink to="/" exact activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>Home</NavLink>
           <NavLink to="/about" activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>About</NavLink>
-          {/* <NavLink to="/course" activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>Courses</NavLink> */}
           <NavLink to="/gallery" activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>Gallery</NavLink>
           <NavLink to="/contact" activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>Contact</NavLink>
-          {/* <NavLink to="/reviews" activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>Reviews</NavLink>
-          <NavLink to="/team" activeClassName="text-blue-500" className="block p-4 hover:bg-gray-100" onClick={toggleMenu}>Team</NavLink> */}
           <div className="p-4">
             <span className="block text-gray-600 mb-2 font-bold">
-              <FaPhone className="mr-2 inline font-bold" /> +91 95 66 86 65 66
+              <FaPhone className="mr-2 inline font-bold" /> +91 9566866588
             </span>
             <span className="block text-gray-600 mb-2 font-bold">
-              <FaEnvelope className="mr-2 inline font-bold" /> nadanaloga@gmail.com
+              <FaEnvelope className="mr-2 inline font-bold" /> nadanalogaa@gmail.com
             </span>
             <div className="flex space-x-4 mb-2">
               <button onClick={() => window.open("https://www.facebook.com/bharathanaatiyam?mibextid=ZbWKwL")} className="text-blue-700 hover:text-blue-900 focus:outline-none">
                 <FaFacebook size={20} style={{ color: '#1877F2' }} />
               </button>
-              {/* <button onClick={() => window.open("https://twitter.com")} className="text-blue-400 hover:text-blue-600 focus:outline-none">
-                <FaTwitter size={20} style={{ color: '#1DA1F2' }} />
-              </button> */}
               <button onClick={() => window.open("https://www.instagram.com/nadanaloga_chennai?igsh=MTNva2wxNGYxaGl2Nw==")} className="text-pink-600 hover:text-pink-800 focus:outline-none">
                 <FaInstagram size={20} style={{ color: '#E1306C' }} />
               </button>
@@ -95,10 +79,6 @@ const Navbar = () => {
                 <FaYoutube size={20} style={{ color: '#FF0000' }} />
               </button>
             </div>
-            {/* <div className="flex space-x-4">
-              <button className="bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 w-full">Login</button>
-              <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full">Register</button>
-            </div> */}
           </div>
         </nav>
       )}
